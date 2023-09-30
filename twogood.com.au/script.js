42c345f1c1cbc280997e4a6e837190c6b772b901
Shalin0003 <146533315+Shalin0003@users.noreply.github.com>
2023-09-30 15:27:20 +0530

    Add files via upload

---
+++ twogood.com.au/script.js
@@ -0,0 +1,7 @@
+var videocon = document.querySelector("#video-container")
+var playbtn = document.querySelector("#play")
+videocon.addEventListener("mouseenter", function(){
+    gsap.to(playbtn, {
+
+    })
+})
