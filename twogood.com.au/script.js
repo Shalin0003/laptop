c6048bdb11b9053ef68297f3075f5e0ad71856b1
Shalin0003 <146533315+Shalin0003@users.noreply.github.com>
2023-10-01 20:21:17 +0530

    Delete twogood.com.au directory

--- twogood.com.au/script.js
+++
@@ -1,7 +0,0 @@
-var videocon = document.querySelector("#video-container")
-var playbtn = document.querySelector("#play")
-videocon.addEventListener("mouseenter", function(){
-    gsap.to(playbtn, {
-
-    })
-})
